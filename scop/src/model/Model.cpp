41ed28aada20ca44434a25184b9f0da47947a684
ElMariuso <thiry.marius@gmail.com>
2024-01-21 15:21:51 +0100

    Finish vertices adding

--- scop/src/model/Model.cpp
+++ scop/src/model/Model.cpp
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 22:25:48 by mthiry            #+#    #+#             */
-/*   Updated: 2024/01/20 17:36:40 by mthiry           ###   ########.fr       */
+/*   Updated: 2024/01/21 15:21:15 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,16 +70,63 @@ void Model::parseOBJ()
         else if (lineType == "f")
         {
             std::vector<Vertex> faceVertices;
-            std::string vertexSpec;
-
+            std::string         vertexSpec;
+        
             while (lineStream >> vertexSpec)
             {
-                std::istringstream  vertexStream(vertexSpec);
-                Vertex              vertex;
+                std::istringstream                  vertexStream(vertexSpec);
+                std::vector<glm::vec3>::size_type   posIndex;
+                std::vector<glm::vec2>::size_type   texIndex = 0;
+                std::vector<glm::vec3>::size_type   normIndex = 0;
+                
+                char slash;
+                vertexStream >> posIndex >> std::noskipws >> slash;
+                
+                if (vertexStream.peek() != '/') {
+                    vertexStream >> texIndex;
+                }
+                vertexStream >> slash;
+                if (vertexStream.peek() != '/') {
+                    vertexStream >> normIndex;
+                }
 
-                std::cout << vertexSpec << std::endl;
+                // std::cout << "POS: " << posIndex << std::endl;
+                // std::cout << "TEX: " << texIndex << std::endl;
+                // std::cout << "NORM: " << normIndex << std::endl << std::endl;
+
+                Vertex  vertex;
+
+                if (posIndex > 0 && posIndex <= positions.size())
+                    vertex.position = positions[posIndex - 1];
+                if (texIndex > 0 && texIndex <= texCoords.size())
+                    vertex.texcoord = texCoords[texIndex - 1];
+                if (normIndex > 0 && normIndex <= normals.size())
+                    vertex.normal = normals[normIndex - 1];
+                
+                faceVertices.push_back(vertex);
             }
-            break ;
+            for (size_t i = 1; i + 1 < faceVertices.size(); ++i) {
+                this->vertices.push_back(faceVertices[0]);
+                this->vertices.push_back(faceVertices[i]);
+                this->vertices.push_back(faceVertices[i + 1]);
+            }
+
+            // for (const auto& vertex : this->vertices) {
+            //     std::cout << "Position: "
+            //         << vertex.position.x << ", "
+            //         << vertex.position.y << ", "
+            //         << vertex.position.z << " | ";
+
+            //     std::cout << "Texcoord: "
+            //         << vertex.texcoord.x << ", "
+            //         << vertex.texcoord.y << " | ";
+
+            //     std::cout << "Normal: "
+            //         << vertex.normal.x << ", "
+            //         << vertex.normal.y << ", "
+            //         << vertex.normal.z << std::endl;
+            // }
+
         }
     }
 }
