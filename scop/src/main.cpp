69dbaf4ae0dc9f1bcbfe78de3022866f7860073b
ElMariuso <thiry.marius@gmail.com>
2024-12-29 18:53:25 +0100

    Clean from scratch

--- scop/src/main.cpp
+++ scop/src/main.cpp
@@ -6,21 +6,11 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 21:37:48 by mthiry            #+#    #+#             */
-/*   Updated: 2024/03/12 02:06:08 by mthiry           ###   ########.fr       */
+/*   Updated: 2024/12/29 18:53:08 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "config.hpp"
-#include "classes.hpp"
-
 int main()
 {
-    // Render render;
-
-    // render.run();
-
-    Model   test("models/obj/42.obj");
-
-    test.loadModel();
     return (0);
 }
